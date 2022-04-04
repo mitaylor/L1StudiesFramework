dc79544cacfb7d76549e781723ab50e146f17799
mitaylor <mitay@mit.edu>
2022-04-04 11:30:24 -0400

    RunL1Ntuples: update example file name

--- RunL1Ntuples/runCmsDriver_2018Data.sh
+++ RunL1Ntuples/runCmsDriver_2018Data.sh
@@ -2,7 +2,7 @@
 
 globaltag=123X_dataRun2_v1 # for Run 2 data reprocessing
 era=Run2_2018_pp_on_AA # for Run 2
-filein='file:/eos/cms/store/group/phys_heavyions/mitaylor/L1EmulatorTestFiles/MinimumBias.root' # change to desired test file
+filein='file:/eos/cms/store/group/phys_heavyions/mitaylor/L1EmulatorTestFiles/MinimumBias_Run326295.root' # change to desired test file
 config=L1Ntuple_Data2018; # cmsRun config file name
 
 cmsDriver.py l1Ntuple -s RAW2DIGI --no_exec --repacked --python_filename=${config}.py \
