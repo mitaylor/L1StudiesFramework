595f7d756a8db586351ee2494ded18b7fdf18e27
mitaylor <mitay@mit.edu>
2023-07-19 12:55:12 -0400

    ReadL1Ntuples: update to includ menu v0_0_1

--- ReadL1Ntuples/src/PrintDecisions.C
+++ ReadL1Ntuples/src/PrintDecisions.C
@@ -10,6 +10,8 @@ Input:  L1Ntuple made with one of the following menu versions
     L1Menu_CollisionsHeavyIons2022_v1_0_0.xml --> Menu::Y2022_V1_0_0
     L1Menu_CollisionsHeavyIons2022_v1_0_1.xml --> Menu::Y2022_V1_0_1
     L1Menu_CollisionsHeavyIons2022_v1_1_0.xml --> Menu::Y2022_V1_1_0
+    L1Menu_CollisionsHeavyIons2023_v0_0_0.xml --> Menu::Y2023_V0_0_0
+    L1Menu_CollisionsHeavyIons2023_v0_0_1.xml --> Menu::Y2023_V0_0_1
 Output: A list of how many times each menu trigger was passed
 Note: If you are using your own custom menu, add it to Menu.h following the instructions in data/README
 */
@@ -29,7 +31,7 @@ using namespace std;
 
 int Print(char const* input) {
     /* initilaize menu */
-    Menu menu(Menu::Y2022_V1_1_0);
+    Menu menu(Menu::Y2023_V0_0_1);
 
     /* file, trees, and branches */
     TFile* inFile = new TFile(input, "read");
