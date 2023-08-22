53a9842f3ba647307db73d78c76bde8e1c93523c
mitaylor <mitay@mit.edu>
2023-08-22 08:42:18 -0400

    RunPPForest: create L1Ntuples and offline information for 2023 PP reference MC

---
+++ RunPPForest/README.md
@@ -0,0 +1,63 @@
+## Instructions to run the L1Emulator + HIForest with the Run 3 PP reference menu using CMSSW_13_2_0_pre3
+
+These instructions are for running the L1Ntuples with the offline information included. 
+
+These configs do not enable resimulating HCal TPs, which means that the Minimum Bias trigger thresholds will not be altered from when the data was taken or the MC was generated. If you would like to change the MB thresholds, contact me and I will tell you which lines to alter in the cmsRun configs (currently they are commented out).
+
+### 1. Set up the emulator
+
+```
+cmsrel CMSSW_13_2_0_pre3
+cd CMSSW_13_2_0_pre3/src
+cmsenv
+git cms-init
+git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git
+git fetch cms-l1t-offline l1t-integration-CMSSW_13_2_0_pre3
+git cms-merge-topic -u cms-l1t-offline:l1t-integration-v164-CMSSW_13_2_0_pre3
+git clone https://github.com/cms-l1t-offline/L1Trigger-L1TCalorimeter.git L1Trigger/L1TCalorimeter/data
+git cms-merge-topic CmsHI:forest_CMSSW_13_2_X
+git remote add cmshi git@github.com:CmsHI/cmssw.git
+svn export https://github.com/boundino/HltL1Run2021.git/trunk/L1/ADC
+
+git cms-checkdeps -A -a
+
+scram b -j 8
+```
+
+### 2. Add the Run 3 PP reference menu to the emulator
+
+```
+git cms-addpkg L1Trigger/L1TCommon
+git cms-addpkg L1Trigger/L1TGlobal
+mkdir -p L1Trigger/L1TGlobal/data/Luminosity/startup/
+cd L1Trigger/L1TGlobal/data/Luminosity/startup/
+wget wget https://raw.githubusercontent.com/mitaylor/HIMenus/main/Menus/L1Menu_CollisionsPPRef2023_v0_0_2.xml
+cd ../../../../../
+scram b -j 8
+```
+
+Edit the file L1Trigger/Configuration/python/customiseUtils.py by changing the L1TriggerMenuFile: process.TriggerMenu.L1TriggerMenuFile = cms.string('L1Menu_Collisions2022_v1_2_0.xml') → process.TriggerMenu.L1TriggerMenuFile = cms.string('L1Menu_CollisionsPPRef2023_v0_0_2.xml')
+
+### 3. Do a local test of cmsRun
+
+For the following instructions please alter the paths to reflect your own setup.
+
+**For processing Run 3 MC:**
+
+```
+cp L1StudiesFramework/RunPPL1Ntuples/forestL1_PP_Run3MC.py CMSSW_13_2_0_pre3/src
+cd CMSSW_13_2_0_pre3/src
+cmsRun forestL1_PP_Run3MC.py
+```
+
+### 4. Submit CRAB jobs
+
+Edit crabConfig_forestL1_PP_Run3MC.py to input your storage area, storage site, dataset, and job name. Then for the following instructions please alter the paths to reflect your own setup.
+
+**For processing Run 3 MC:**
+
+```
+cp L1StudiesFramework/RunPPL1Ntuples/crabConfig_forestL1_PP_Run3MC.py CMSSW_13_2_0_pre3/src
+cd CMSSW_13_2_0_pre3/src
+crab submit -c crabConfig_forestL1_PP_Run3MC.py
+```
---
+++ RunPPForest/forestL1_PP_Run3MC.py
@@ -0,0 +1,268 @@
+### HiForest Configuration
+# Collisions: pp
+# Type: MC
+# Input: miniAOD
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Era_Run3_cff import Run3
+process = cms.Process('HiForest', Run3)
+
+#####################################################################################
+# HiForest labeling info
+#####################################################################################
+
+process.load("HeavyIonsAnalysis.EventAnalysis.HiForestInfo_cfi")
+process.HiForestInfo.info = cms.vstring("HiForest, miniAOD, 132X, mc")
+
+#####################################################################################
+# Input source
+#####################################################################################
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/user/soohwan/RECO_MINIAOD_MC_QCDDiJetsPU_forPPRef_CMSSW_13_2_0_pre1_10Jun2023_v1/Dijet_TuneCP5_5p36TeV_ppref-pythia8/RECO_MINIAOD_MC_QCDDiJetsPU_forPPRef_CMSSW_13_2_0_pre1_10Jun2023_v1/230615_190649/0000/step3_RAW2DIGI_L1Reco_RECO_RECOSIM_PAT_1.root'
+    ),
+    secondaryFileNames = cms.untracked.vstring(
+        '/store/user/soohwan/DIGIRAW_MC_QCDJets_forPPRef_CMSSW_13_2_0_pre1_10Jun2023_v1/Dijet_TuneCP5_5p36TeV_ppref-pythia8/DIGIRAW_MC_QCDJets_forPPRef_CMSSW_13_2_0_pre1_10Jun2023_v1/230611_154057/0000/JME-RunIIAutumn18DR-00003_step1_1.root'
+    )
+)
+
+# Number of events we want to process, -1 = all events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(200)
+)
+
+#####################################################################################
+# Load Global Tag, geometry, etc.
+#####################################################################################
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.Geometry.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2023_realistic', '')
+process.HiForestInfo.GlobalTagLabel = process.GlobalTag.globaltag
+
+# Old calibration here, might need to update
+process.GlobalTag.toGet.extend([
+    cms.PSet(record = cms.string("BTagTrackProbability3DRcd"),
+             tag = cms.string("JPcalib_MC94X_2017pp_v2"),
+             connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS")
+         )
+])
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('l1Ntuple nevts:10000'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+#####################################################################################
+# Define tree output
+#####################################################################################
+
+# root output
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("HiForestMiniAOD.root"))
+
+# # edm output for debugging purposes
+# process.output = cms.OutputModule(
+#     "PoolOutputModule",
+#     fileName = cms.untracked.string('HiForestEDM.root'),
+#     outputCommands = cms.untracked.vstring(
+#         'keep *',
+#         )
+#     )
+
+# process.output_path = cms.EndPath(process.output)
+
+#########################
+# Gen analyzer
+#########################
+
+process.load('HeavyIonsAnalysis.EventAnalysis.HiGenAnalyzer_cfi')
+
+# make cuts looser so that we can actually check dNdEta
+process.HiGenParticleAna.ptMin = cms.untracked.double(0.4) # default is 5
+process.HiGenParticleAna.etaMax = cms.untracked.double(5.) # default is 2.5
+
+#########################
+# Event Analysis
+#########################
+
+# use data version to avoid PbPb MC
+process.load('HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_data_cfi')
+process.hiEvtAnalyzer.Vertex = cms.InputTag("offlineSlimmedPrimaryVertices")
+process.hiEvtAnalyzer.doCentrality = cms.bool(False)
+process.hiEvtAnalyzer.doEvtPlane = cms.bool(False)
+process.hiEvtAnalyzer.doEvtPlaneFlat = cms.bool(False)
+process.hiEvtAnalyzer.doMC = cms.bool(True) # general MC info
+process.hiEvtAnalyzer.doHiMC = cms.bool(False) # HI specific MC info
+process.hiEvtAnalyzer.doHFfilters = cms.bool(False) # Disable HF filters for ppRef
+
+# process.load('HeavyIonsAnalysis.EventAnalysis.hltanalysis_cfi')
+process.load('HeavyIonsAnalysis.EventAnalysis.hltobject_cfi')
+process.load('HeavyIonsAnalysis.EventAnalysis.l1object_cfi')
+
+from HeavyIonsAnalysis.EventAnalysis.hltobject_cfi import trigger_list_mc
+process.hltobject.triggerNames = trigger_list_mc
+
+#########################
+# Photons and electrons
+#########################
+
+process.load('HeavyIonsAnalysis.EGMAnalysis.ggHiNtuplizer_cfi')
+process.ggHiNtuplizer.doGenParticles = cms.bool(True)
+process.ggHiNtuplizer.doMuons = cms.bool(False) # unpackedMuons collection not found from file
+process.ggHiNtuplizer.useValMapIso = cms.bool(False) # True here causes seg fault
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+
+#########################
+# Jets
+#########################
+
+process.load("HeavyIonsAnalysis.JetAnalysis.ak4PFJetSequence_ppref_mc_cff")
+
+#########################
+# Tracks
+#########################
+
+# Tracks cause an error due to missing product: packedPFCandidateTrackChi2
+# process.load('HeavyIonsAnalysis.TrackAnalysis.TrackAnalyzers_cff')
+
+#########################
+# Main analysis list
+#########################
+
+process.forest = cms.Path(
+    process.HiForestInfo +
+    # process.hltanalysis *
+    process.hiEvtAnalyzer *
+    process.hltobject +
+    process.l1object +
+    process.HiGenParticleAna +
+    process.ggHiNtuplizer #+
+#    process.trackSequencePP
+)
+
+#########################
+# Customization
+#########################
+
+addR3Jets = False
+addR4Jets = True
+
+if addR3Jets or addR4Jets :
+    process.load("HeavyIonsAnalysis.JetAnalysis.extraJets_cff")
+    from HeavyIonsAnalysis.JetAnalysis.clusterJetsFromMiniAOD_cff import setupPprefJets
+
+    if addR3Jets :
+        process.jetsR3 = cms.Sequence()
+        setupPprefJets('ak3PF', process.jetsR3, process, isMC = 1, radius = 0.30, JECTag = 'AK3PF')
+        process.ak3PFpatJetCorrFactors.levels = ['L2Relative', 'L3Absolute']
+        process.load("HeavyIonsAnalysis.JetAnalysis.candidateBtaggingMiniAOD_cff")
+        process.ak3PFJetAnalyzer = process.ak4PFJetAnalyzer.clone(jetTag = "ak3PFpatJets", jetName = 'ak3PF', genjetTag = "ak3GenJetsNoNu")
+        process.forest += process.extraJetsMC * process.jetsR3 * process.ak3PFJetAnalyzer
+
+    if addR4Jets :
+        # Recluster using an alias "0" in order not to get mixed up with the default AK4 collections
+        process.jetsR4 = cms.Sequence()
+        setupPprefJets('ak04PF', process.jetsR4, process, isMC = 1, radius = 0.40, JECTag = 'AK4PF')
+        process.ak04PFpatJetCorrFactors.levels = ['L2Relative', 'L3Absolute']
+        process.ak04PFpatJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
+        process.load("HeavyIonsAnalysis.JetAnalysis.candidateBtaggingMiniAOD_cff")
+        process.ak4PFJetAnalyzer.jetTag = 'ak04PFpatJets'
+        process.ak4PFJetAnalyzer.jetName = 'ak04PF'
+        process.ak4PFJetAnalyzer.doSubEvent = False # Need to disable this, since there is some issue with the gen jet constituents. More debugging needed is want to use constituents.
+        process.forest += process.extraJetsMC * process.jetsR4 * process.ak4PFJetAnalyzer
+
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.forest,process.raw2digi_step,process.endjob_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#####################################################################################
+# L1 emulation
+#####################################################################################
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseReEmul
+# from L1Trigger.Configuration.customiseReEmul import L1TReEmulMCFromRAWSimHcalTP
+from L1Trigger.Configuration.customiseReEmul import L1TReEmulMCFromRAW
+
+# Call to customisation function L1TReEmulMCFromRAW imported from L1Trigger.Configuration.customiseReEmul
+# process = L1TReEmulMCFromRAWSimHcalTP(process)
+process = L1TReEmulMCFromRAW(process)
+
+# Automatic addition of the customisation function from L1Trigger.L1TNtuples.customiseL1Ntuple
+from L1Trigger.L1TNtuples.customiseL1Ntuple import L1NtupleRAWEMU 
+
+# Call to customisation function L1NtupleRAWEMU imported from L1Trigger.L1TNtuples.customiseL1Ntuple
+process = L1NtupleRAWEMU(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseSettings
+from L1Trigger.Configuration.customiseSettings import L1TSettingsToCaloParamsHI_2023_v0_3 
+
+# Call to customisation function L1TSettingsToCaloParamsHI_2023_v0_3 imported from L1Trigger.Configuration.customiseSettings
+process = L1TSettingsToCaloParamsHI_2023_v0_3(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customiseUtils
+from L1Trigger.Configuration.customiseUtils import L1TGlobalMenuXML 
+
+# Call to customisation function L1TGlobalMenuXML imported from L1Trigger.Configuration.customiseUtils
+process = L1TGlobalMenuXML(process)
+
+#########################
+# Customization
+#########################
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
